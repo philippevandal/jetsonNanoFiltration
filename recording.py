3def798732cefda662ec05a0737c794f39cebfe5
philippevandal <34427212+philippevandal@users.noreply.github.com>
2022-01-01 18:35:55 -0500

    Add files via upload

--- recording.py
+++ recording.py
@@ -8,17 +8,19 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(button, GPIO.IN)
 
 Cap = cv2.VideoCapture(0)
-filename = input("Enter name of input file: ")
-writerC = cv2.VideoWriter('/videos/{filename}.mp4', cv2.VideoWriter_fourcc(*'XVID'),8, (1280, 960))
+#filename = input("Enter name of output file: ")
+writerC = cv2.VideoWriter('videos/test.mp4', cv2.VideoWriter_fourcc(*'mp4v'),15.0, (1280, 960))
 
 while True:
-    ret, frameC = Cap.read()     
+    ret, frameC = Cap.read()
     frameC = cv2.resize(frameC,(1280, 960))
     writerC.write(frameC)
     # cv2.imshow('frameC', frameC)
-    frame = None
+    #frame = None
     #if GPIO.input(button):
     key = cv2.waitKey(30) & 0xff
     if key == 27:
         #GPIO.cleanup()
         break
+Cap.release()
+writerC.release()
